03dcd788832132eb569423584dcb588858d718a5
SharkFishDeveloper <shahzebakhtart66@gmail.com>
2026-05-24 21:10:32 +0530

    a

--- utils/backendUrl.ts
+++ utils/backendUrl.ts
@@ -1,2 +1,2 @@
-export  const  BACKEND_URL:string = "http://localhost:3000";
+export  const  BACKEND_URL:string = "https://bd-mentor.onrender.com";
 export const univerOptions = ["Thapar","Harward","MIT","IIT-B","IIT-D","IIT-K","Stanford"];
